ogournet/grout

commit 4c5ccc84ef36e9c906d77465b2e2924ea14b7eaa

ip6: make route_lookup_exact private
The ip6_route_lookup_exact function is only used in route.c, make it
static and remove it from the headers.

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/ip6/control/gr_ip6_control.h
@@ -37,12 +37,6 @@ int ip6_route_delete(
 );
 void ip6_route_cleanup(struct nexthop *);
 struct nexthop *ip6_route_lookup(uint16_t vrf_id, uint16_t iface_id, const struct rte_ipv6_addr *);
-struct nexthop *ip6_route_lookup_exact(
-	uint16_t vrf_id,
-	uint16_t iface_id,
-	const struct rte_ipv6_addr *,
-	uint8_t prefixlen
-);
 
 // get the default address for a given interface
 struct nexthop *ip6_addr_get_preferred(uint16_t iface_id, const struct rte_ipv6_addr *);

modules/ip6/control/route.c
@@ -127,7 +127,7 @@ ip6_route_lookup(uint16_t vrf_id, uint16_t iface_id, const struct rte_ipv6_addr 
 	return nh_id_to_ptr(nh_id);
 }
 
-struct nexthop *ip6_route_lookup_exact(
+static struct nexthop *ip6_route_lookup_exact(
 	uint16_t vrf_id,
 	uint16_t iface_id,
 	const struct rte_ipv6_addr *ip,

commit 281e12cd14a39d34e4d568218a6fafb537a5ea3f

ip6: make route_lookup_exact public
It will be needed by other modules that manipulate routes.

Fixes: 4c5ccc84ef36 ("ip6: make route_lookup_exact private")
Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

modules/ip6/control/gr_ip6_control.h
@@ -35,6 +35,8 @@ int rib6_delete(
 );
 void rib6_cleanup(struct nexthop *);
 struct nexthop *rib6_lookup(uint16_t vrf_id, uint16_t iface_id, const struct rte_ipv6_addr *);
+struct nexthop *rib6_lookup_exact(uint16_t vrf_id, uint16_t iface_id, const struct rte_ipv6_addr *ip,
+				  uint8_t prefixlen);
 
 // get the default address for a given interface
 struct nexthop *addr6_get_preferred(uint16_t iface_id, const struct rte_ipv6_addr *);

modules/ip6/control/route.c
@@ -121,7 +121,7 @@ struct nexthop *rib6_lookup(uint16_t vrf_id, uint16_t iface_id, const struct rte
 	return nh_id_to_ptr(nh_id);
 }
 
-static struct nexthop *rib6_lookup_exact(
+struct nexthop *rib6_lookup_exact(
 	uint16_t vrf_id,
 	uint16_t iface_id,
 	const struct rte_ipv6_addr *ip,