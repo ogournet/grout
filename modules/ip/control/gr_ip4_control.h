ogournet/grout

commit 1e80bd6a450610a44bc6cb36e6cdcc1c0cfbbaf3

ip: make route_lookup_exact private
The ip4_route_lookup_exact function is only used in route.c, make it
static and remove it from the headers.

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/ip/control/gr_ip4_control.h
@@ -28,7 +28,6 @@ void arp_probe_input_cb(struct rte_mbuf *m);
 int ip4_route_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, struct nexthop *);
 int ip4_route_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
 struct nexthop *ip4_route_lookup(uint16_t vrf_id, ip4_addr_t ip);
-struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
 void ip4_route_cleanup(struct nexthop *);
 
 // get the default address for a given interface

modules/ip/control/route.c
@@ -121,7 +121,7 @@ struct nexthop *ip4_route_lookup(uint16_t vrf_id, ip4_addr_t ip) {
 	return nh_id_to_ptr(nh_id);
 }
 
-struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen) {
+static struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen) {
 	uint32_t host_order_ip = rte_be_to_cpu_32(ip);
 	struct rte_fib *fib = get_fib(vrf_id);
 	struct rte_rib_node *rn;

commit 6f6f0b7d97eeb8f61b5f42a16558244f19a12cab

ip: make route_lookup_exact public
It will be needed by other modules that manipulate routes.

Fixes: 1e80bd6a4506 ("ip: make route_lookup_exact private")
Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

modules/ip/control/gr_ip4_control.h
@@ -25,6 +25,7 @@ void nh4_unreachable_cb(struct rte_mbuf *m);
 void arp_probe_input_cb(struct rte_mbuf *m);
 
 struct nexthop *rib4_lookup(uint16_t vrf_id, ip4_addr_t ip);
+struct nexthop *rib4_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
 int rib4_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, struct nexthop *);
 int rib4_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
 void rib4_cleanup(struct nexthop *);

modules/ip/control/route.c
@@ -115,7 +115,7 @@ struct nexthop *rib4_lookup(uint16_t vrf_id, ip4_addr_t ip) {
 	return nh_id_to_ptr(nh_id);
 }
 
-static struct nexthop *rib4_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen) {
+struct nexthop *rib4_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen) {
 	struct rte_rib *rib = get_rib(vrf_id);
 	struct rte_rib_node *rn;
 	uintptr_t nh_id;