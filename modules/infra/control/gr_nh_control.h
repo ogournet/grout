ogournet/grout

commit 66289dfd03058dd2132e77a59d96691603ec3253

nexthop: may visit a custom node on ip_input and ip6_input
This is a fast and simple way of entering module's nodes,
right after route_lookup() in ip_input or ip6_input.

The nexthop object inserted in rib4/rib6 should set the
input_node field to its node. For example:

nh = nexthop_new(...);
nh->input_node = gr_node_attach_parent("ip6_input", "my_input_node");
rib6_insert(..., nh);

Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

modules/infra/control/gr_nh_control.h
@@ -10,6 +10,8 @@
 #include <event2/event.h>
 #include <rte_mbuf.h>
 
+#include <rte_graph.h>
+
 // Forward declaration
 struct nexthop;
 
@@ -82,6 +84,9 @@ struct __rte_cache_aligned nexthop {
 	uint16_t held_pkts_num;
 	struct rte_mbuf *held_pkts_head;
 	struct rte_mbuf *held_pkts_tail;
+
+	// custom node to visit on packet input
+	rte_edge_t input_node;
 
 	// internal
 	struct nh_pool *pool;

modules/infra/control/nexthop.c
@@ -137,6 +137,7 @@ nexthop_new(struct nh_pool *nhp, uint16_t vrf_id, uint16_t iface_id, const void 
 		break;
 	}
 	nh->pool = nhp;
+	nh->input_node = RTE_EDGE_ID_INVALID;
 
 	nexthop_push_notification(NEXTHOP_EVENT_NEW, nh);
 	return nh;

modules/ip/datapath/ip_input.c
@@ -114,6 +114,8 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 			edge = LOCAL;
 		else if (e->domain == ETH_DOMAIN_LOOPBACK)
 			edge = OUTPUT;
+		else if (nh->input_node != RTE_EDGE_ID_INVALID)
+			edge = nh->input_node;
 		else
 			edge = FORWARD;
 next:

modules/ip6/datapath/ip6_input.c
@@ -112,6 +112,8 @@ ip6_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 			edge = LOCAL;
 		else if (e->domain == ETH_DOMAIN_LOOPBACK)
 			edge = OUTPUT;
+		else if (nh->input_node != RTE_EDGE_ID_INVALID)
+			edge = nh->input_node;
 		else
 			edge = FORWARD;
 

commit 56cc9b63fb0bfb780f703fe34cef79b698208e5c

nexthop: add hooks based on nexthop family
Add a possibility to visit registered nodes, instead of FORWARD,
in ip_input and ip6_input. Nodes are registered based on nexthop family.

Fixes: 66289dfd0 ("nexthop: may visit a custom node on ip_input and ip6_input")
Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

modules/infra/control/gr_nh_control.h
@@ -85,9 +85,6 @@ struct __rte_cache_aligned nexthop {
 	struct rte_mbuf *held_pkts_head;
 	struct rte_mbuf *held_pkts_tail;
 
-	// custom node to visit on packet input
-	rte_edge_t input_node;
-
 	// internal
 	struct nh_pool *pool;
 };

modules/infra/control/nexthop.c
@@ -137,7 +137,6 @@ nexthop_new(struct nh_pool *nhp, uint16_t vrf_id, uint16_t iface_id, const void 
 		break;
 	}
 	nh->pool = nhp;
-	nh->input_node = RTE_EDGE_ID_INVALID;
 
 	nexthop_push_notification(NEXTHOP_EVENT_NEW, nh);
 	return nh;

modules/ip/datapath/gr_ip4_datapath.h
@@ -29,6 +29,7 @@ GR_MBUF_PRIV_DATA_TYPE(ip_local_mbuf_data, {
 	uint8_t ttl;
 });
 
+void ip_input_register_family(uint8_t family, const char *next_node);
 void ip_input_local_add_proto(uint8_t proto, const char *next_node);
 void ip_output_register_interface(uint16_t iface_type_id, const char *next_node);
 int arp_output_request_solicit(struct nexthop *nh);

modules/ip/datapath/ip_input.c
@@ -29,6 +29,14 @@ enum edges {
 	EDGE_COUNT,
 };
 
+static rte_edge_t edges[256] = {FORWARD};
+
+void ip_input_register_family(uint8_t family, const char *next_node) {
+	if (edges[family] != FORWARD)
+		ABORT("next node already registered for family=%u", family);
+	edges[family] = gr_node_attach_parent("ip_input", next_node);
+}
+
 static uint16_t
 ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct eth_input_mbuf_data *e;
@@ -114,10 +122,8 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 			edge = LOCAL;
 		else if (e->domain == ETH_DOMAIN_LOOPBACK)
 			edge = OUTPUT;
-		else if (nh->input_node != RTE_EDGE_ID_INVALID)
-			edge = nh->input_node;
 		else
-			edge = FORWARD;
+			edge = edges[nh->family];
 next:
 		if (gr_mbuf_is_traced(mbuf)) {
 			struct rte_ipv4_hdr *t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));

modules/ip6/datapath/gr_ip6_datapath.h
@@ -33,6 +33,7 @@ GR_MBUF_PRIV_DATA_TYPE(ndp_na_output_mbuf_data, {
 	const struct nexthop *remote;
 });
 
+void ip6_input_register_family(uint8_t family, const char *next_node);
 void ip6_input_local_add_proto(uint8_t proto, const char *next_node);
 void ip6_output_register_interface(uint16_t iface_type_id, const char *next_node);
 int nh6_solicit(struct nexthop *nh);

modules/ip6/datapath/ip6_input.c
@@ -31,6 +31,14 @@ enum edges {
 	EDGE_COUNT,
 };
 
+static rte_edge_t edges[256] = {FORWARD};
+
+void ip6_input_register_family(uint8_t family, const char *next_node) {
+	if (edges[family] != FORWARD)
+		ABORT("next node already registered for family=%u", family);
+	edges[family] = gr_node_attach_parent("ip6_input", next_node);
+}
+
 static uint16_t
 ip6_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct ip6_output_mbuf_data *d;
@@ -112,10 +120,8 @@ ip6_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 			edge = LOCAL;
 		else if (e->domain == ETH_DOMAIN_LOOPBACK)
 			edge = OUTPUT;
-		else if (nh->input_node != RTE_EDGE_ID_INVALID)
-			edge = nh->input_node;
 		else
-			edge = FORWARD;
+			edge = edges[nh->family];
 
 next:
 		if (gr_mbuf_is_traced(mbuf)) {

modules/srv6/control.c
@@ -77,7 +77,7 @@ static struct api_out srv6_steer_add(const void *request, void ** /*response*/) 
 		nh = nh6_new(req->s.vrf_id, GR_IFACE_ID_UNDEF, &req->s.dest6.ip);
 		if (nh == NULL)
 			return api_out(errno, 0);
-		nh->input_node = srv6_steer_v6_edge;
+		nh->family = GR_AF_SRV6_STEER;
 		nh->prefixlen = req->s.dest6.prefixlen;
 
 		ret = rib6_insert(req->s.vrf_id, GR_IFACE_ID_UNDEF, &req->s.dest6.ip,
@@ -93,7 +93,7 @@ static struct api_out srv6_steer_add(const void *request, void ** /*response*/) 
 		nh = nh4_new(req->s.vrf_id, GR_IFACE_ID_UNDEF, req->s.dest4.ip);
 		if (nh == NULL)
 			return api_out(errno, 0);
-		nh->input_node = srv6_steer_v4_edge;
+		nh->family = GR_AF_SRV6_STEER;
 		nh->prefixlen = req->s.dest4.prefixlen;
 
 		ret = rib4_insert(req->s.vrf_id, req->s.dest4.ip,
@@ -303,7 +303,7 @@ static struct api_out srv6_localsid_add(const void *request, void ** /*response*
 	nh = nh6_new(req->l.vrf_id, GR_IFACE_ID_UNDEF, &req->l.lsid);
 	if (nh == NULL)
 		return api_out(errno, 0);
-	nh->input_node = srv6_local_edge;
+	nh->family = GR_AF_SRV6_LOCAL;
 	nh->flags |= GR_NH_F_STATIC | GR_NH_F_REACHABLE;
 
 	// and add it to the routing table (/128), so ip6_input fib will

modules/srv6/datapath_local.c
@@ -209,7 +209,7 @@ next:
 }
 
 static void srv6_node_init(void) {
-	srv6_local_edge = gr_node_attach_parent("ip6_input", "srv6_local");
+	ip6_input_register_family(GR_AF_SRV6_LOCAL, "srv6_local");
 }
 
 

modules/srv6/datapath_steer.c
@@ -29,9 +29,6 @@ enum {
 	EDGE_COUNT,
 };
 
-rte_edge_t srv6_steer_v4_edge;
-rte_edge_t srv6_steer_v6_edge;
-
 struct trace_srv6_data {
 	union {
 		struct ip4_net dest4;
@@ -162,8 +159,8 @@ srv6_steer_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 }
 
 static void srv6_steer_register(void) {
-	srv6_steer_v4_edge = gr_node_attach_parent("ip_input", "srv6_steer");
-	srv6_steer_v6_edge = gr_node_attach_parent("ip6_input", "srv6_steer");
+	ip6_input_register_family(GR_AF_SRV6_STEER, "srv6_steer");
+	ip_input_register_family(GR_AF_SRV6_STEER, "srv6_steer");
 }
 
 static struct rte_node_register srv6_steer_node = {

modules/srv6/gr_srv6.h
@@ -9,6 +9,11 @@
 #include <endian.h>
 
 
+// define new nexthop->family
+#define GR_AF_SRV6_LOCAL (AF_MAX)
+#define GR_AF_SRV6_STEER (GR_AF_SRV6_LOCAL + 1)
+
+
 //
 // https://www.iana.org/assignments/segment-routing/segment-routing.xhtml
 //
@@ -61,8 +66,6 @@ struct srv6_localsid_data {
 struct srv6_localsid_data *srv6_localsid_get(const struct rte_ipv6_addr *lsid,
 					     uint16_t vrf_id);
 
-extern rte_edge_t srv6_local_edge;
-
 
 //
 // srv6 data shared between control - srv6_steer node
@@ -74,9 +77,6 @@ struct srv6_steer_data {
 };
 
 struct srv6_steer_data *srv6_steer_get(const struct nexthop *nh);
-
-extern rte_edge_t srv6_steer_v4_edge;
-extern rte_edge_t srv6_steer_v6_edge;
 
 
 #endif